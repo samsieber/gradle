a010b057f7d037449f570bb4ed06c54bfe5bbfa8
Daz DeBoer <daz@gradle.com>
2018-08-02 18:23:53 -0600

    Add missing @since tag for new API

--- subprojects/core-api/src/main/java/org/gradle/api/artifacts/MutableVersionConstraint.java
+++ subprojects/core-api/src/main/java/org/gradle/api/artifacts/MutableVersionConstraint.java
@@ -48,6 +48,7 @@ public interface MutableVersionConstraint extends VersionConstraint {
     /**
      * Sets the required version of this module. Any other version constraints will be overriden.
      * @param version the preferred version of this module
+     * @since 4.11
      */
     void require(String version);
 
